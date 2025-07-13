a39847791c43347d317d486f5e171fa70bc308e6
Takunda Madechangu <madechangu.takunda@gmail.com>
2025-07-13 23:45:22 +0200

    Add form to AddAI provider settings

--- src/pages/settings/AddAIProvider.tsx
+++ src/pages/settings/AddAIProvider.tsx
@@ -1,9 +1,17 @@
 
 
 function AddAIProvider() {
+
+
+    const onSubmit = () => {
+        console.log('Submitted');
+    }
+
+
     return (
         <div>
             <h1>Add AI Provider</h1>
+            <AddProviderForm onSubmit={onSubmit} />
         </div>
     );
 }
